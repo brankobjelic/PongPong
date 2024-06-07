ae6e9259b0a44df267d98adcd6b24fa0de4837c9
33072204+brankobjelic@users.noreply.github.com <33072204+brankobjelic@users.noreply.github.com>
2024-06-07 12:34:54 +0200

    Change bounce angle when ball hits a paddle

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,6 +7,7 @@ public class Ball : MonoBehaviour
     //public GameManager gameManager;
     public Rigidbody2D rb2d;
     public float maxInitialAngle = 0.67f;
+    public float maxPaddleBounceAngle = 45f;
     public float moveSpeed = 3f;
     public float moveSpeedMultiplier = 1.05f;
 
@@ -50,6 +51,25 @@ public class Ball : MonoBehaviour
         if(collision.collider.TryGetComponent<Paddle>(out var paddle))
         {
             rb2d.velocity *= moveSpeedMultiplier;
+            AdjustAngle(paddle, collision);
         }
+    }
+
+    private void AdjustAngle(Paddle paddle, Collision2D collision)
+    {
+        //ball bounce angle depending on point on a paddle
+        Debug.Log(paddle.transform.position.y);
+        Debug.Log(collision.otherCollider.transform.position.y);
+        float absoluteDistanceFromCenter = collision.otherCollider.transform.position.y - paddle.transform.position.y;
+        float relativeDistanceFromCenter = absoluteDistanceFromCenter * 2 / paddle.transform.localScale.y;
+        Debug.Log(relativeDistanceFromCenter);
+
+        int angleSign = paddle.IsLeftPaddle() ? 1 : -1;
+        float paddleBounceAngle = relativeDistanceFromCenter * angleSign * maxPaddleBounceAngle;
+        Quaternion rotation = Quaternion.AngleAxis(paddleBounceAngle, Vector3.forward);
+
+        Vector2 direction = paddle.IsLeftPaddle() ? Vector2.right : Vector2.left;
+        rb2d.velocity = rotation * direction * rb2d.velocity.magnitude;
+
     }
 }
