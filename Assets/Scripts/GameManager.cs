686239281a6b9df9f18b5f5fa2deab27976559c2
33072204+brankobjelic@users.noreply.github.com <33072204+brankobjelic@users.noreply.github.com>
2024-06-07 21:35:17 +0200

    Move UI functions from GameManager to GameUI script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance; //singleton
 
+    public GameUI gameUI;
     public int scorePlayer1, scorePlayer2;
-    public ScoreText scoreTextLeft, scoreTextRight;
+    
     public Action onReset;
 
     private void Awake()
@@ -33,21 +34,9 @@ public class GameManager : MonoBehaviour
         if (id == 2)
             scorePlayer2++;
 
-        UpdateScores();
-        HighlightScore(id);
+        gameUI.UpdateScores(scorePlayer1, scorePlayer2);
+        gameUI.HighlightScore(id);
     }
 
-    private void UpdateScores()
-    {
-        scoreTextLeft.SetScore(scorePlayer1);
-        scoreTextRight.SetScore(scorePlayer2);
-    }
 
-    private void HighlightScore(int id)
-    {
-        if (id == 1)
-            scoreTextLeft.Highlight();
-        else
-            scoreTextRight.Highlight();
-    }
 }
---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+    public ScoreText scoreTextPlayer1, scoreTextPlayer2;
+    public GameObject menuObject;
+
+    public void UpdateScores(int scorePlayer1, int scorePlayer2)
+    {
+        scoreTextPlayer1.SetScore(scorePlayer1);
+        scoreTextPlayer2.SetScore(scorePlayer2);
+    }
+
+    public void HighlightScore(int id)
+    {
+        if (id == 1)
+            scoreTextPlayer1.Highlight();
+        else
+            scoreTextPlayer2.Highlight();
+    }
+
+    public void OnStartButtonClicked()
+    {
+        menuObject.SetActive(false);
+    }
+}
