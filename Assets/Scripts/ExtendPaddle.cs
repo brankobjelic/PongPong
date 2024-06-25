1e3b68d6b4f2e029b4c9401c0b93bf0b486e15aa
33072204+brankobjelic@users.noreply.github.com <33072204+brankobjelic@users.noreply.github.com>
2024-06-26 01:58:41 +0200

    Add OnDestroy method

--- Assets/Scripts/ExtendPaddle.cs
+++ Assets/Scripts/ExtendPaddle.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class ExtendPaddle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnDestroy()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/PlusOne.cs
+++ Assets/Scripts/PlusOne.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class PlusOne : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnDestroy()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(this.gameObject);
     }
 }
